0c6eef1046a3376a7e075b961d4836c9c3d76661
vungu <66395785+vungu@users.noreply.github.com>
2020-11-24 14:50:57 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('WHAAAAAT UUUP'));
+app.get('/', (req, res) => res.send('WHAAAAAT UUUP 123456'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
